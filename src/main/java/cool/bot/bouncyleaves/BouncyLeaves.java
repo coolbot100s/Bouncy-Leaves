b61151cdc259604f28fa6a0f8636ba86048cd4a0
coolbot100s <76798835+coolbot100s@users.noreply.github.com>
2024-02-18 18:51:00 -0800

    Project Init

---
+++ src/main/java/cool/bot/bouncyleaves/BouncyLeaves.java
@@ -0,0 +1,17 @@
+package cool.bot.bouncyleaves;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BouncyLeaves extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
