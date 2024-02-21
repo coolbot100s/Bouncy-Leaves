567051c1e5e58cc05b9b19a0faa2eedecb7c490e
coolbot100s <76798835+coolbot100s@users.noreply.github.com>
2024-02-21 01:02:24 -0800

    yeeted debug stuff and added TODOs

--- src/main/java/cool/bot/bouncyleaves/BouncyLeaves.java
+++ src/main/java/cool/bot/bouncyleaves/BouncyLeaves.java
@@ -27,12 +27,11 @@ public final class BouncyLeaves extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this,this);
-
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 
     @EventHandler
@@ -43,30 +42,13 @@ public final class BouncyLeaves extends JavaPlugin implements Listener {
         BoundingBox playerBox = player.getBoundingBox();
         List<Block> readyLeaves = new ArrayList<>();
 
-        // get the block the player is in, and all adjacent blocks, if any are a dripleaf continue
-        // check if the player is inside a dripleaf, or air, if so continue
-        // check if the player's hitbox, at their feet, is colliding with any dripleafs that are at full tilt
-        // if so, execute the jump code below
+        //TODO: ignore event if player was yeeted in the last few ticks
 
 
-
-        // populate list with leaves at full tilt from a 3x3 area around the player
-//        for (Block curBlock : getBlocksInArea(location.clone().add(-1,0,-1),location.clone().add(1,0,1))) {
-//            if (curBlock.getType() == Material.BIG_DRIPLEAF) {
-//                BigDripleaf leaf = (BigDripleaf) block.getBlockData();
-//                if (leaf.getTilt() == BigDripleaf.Tilt.FULL) {
-//                    readyLeaves.add(leaf);
-//                }
-//            }
-//        }
-
-        // ignore event if player was just flung in the last tick or so
-
-        // populate list with leaves at fulltilt that the players feet are colliding with
-        //player.sendMessage(String.valueOf(location.getY())); //DEBUG
+        // Get a list of blocks the player's feet are colliding with
+        //TODO: mayhaps remove the flooring?
         List<Block> blocksUnderFeet = getBlocksInArea(new Location(location.getWorld(),Math.floor(playerBox.getMinX()),location.getY(),Math.floor(playerBox.getMinZ())),new Location(location.getWorld(),Math.floor(playerBox.getMaxX()),location.getY(),Math.floor(playerBox.getMaxZ())));
-
-
+        // Check that list for any BIG_DRIPLEAF at full tilt
         for (Block curBlock : blocksUnderFeet) {
             if (curBlock.getType() == Material.BIG_DRIPLEAF) {
                 BlockData curBlockData = curBlock.getBlockData();
@@ -79,14 +61,10 @@ public final class BouncyLeaves extends JavaPlugin implements Listener {
             }
         }
 
-
-
         // if there are none ignore the event
         if (readyLeaves.isEmpty()) {
             return;
         }
-
-       // player.sendMessage(String.valueOf(readyLeaves.size())); //DEBUG
 
         // check to see if the player is standing in air, or a big leaf, if not ignore the event.
         if (!(block.getType() == Material.BIG_DRIPLEAF || block.getType() == Material.AIR)) {
@@ -114,7 +92,8 @@ public final class BouncyLeaves extends JavaPlugin implements Listener {
             leafData.setTilt(BigDripleaf.Tilt.NONE);
             block.setBlockData(leafData);
 
-            // sound?
+            // sound
+            // TODO: maybe this should only play once when the player is yeeted, rather than by each block? if for each block then we should randomize pitch and volume a bit
             player.playSound(player.getLocation(), Sound.ENTITY_SLIME_ATTACK, 2.0f, 1.0f);
 
         }, 1);
