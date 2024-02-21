1a9f66926fb9d49967ceca8745925caf172c2605
coolbot100s <76798835+coolbot100s@users.noreply.github.com>
2024-02-21 00:02:01 -0800

    Multileaf mode, untested

--- src/main/java/cool/bot/bouncyleaves/BouncyLeaves.java
+++ src/main/java/cool/bot/bouncyleaves/BouncyLeaves.java
@@ -6,13 +6,18 @@ import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.BigDripleaf;
+import org.bukkit.block.data.type.Dripleaf;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.BoundingBox;
 import org.bukkit.util.Vector;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.bukkit.Bukkit.getPluginManager;
 
@@ -32,33 +37,94 @@ public final class BouncyLeaves extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
-        event.getPlayer().sendMessage(event.getPlayer().getLocation().getBlock().getType().toString());
-        if (event.getPlayer().getLocation().getBlock().getType() == Material.BIG_DRIPLEAF) {
-            Player player = event.getPlayer();
-            Location location = player.getLocation();
-            Block block = location.getBlock();
-            BigDripleaf leafData = (BigDripleaf) block.getBlockData();
+        Player player = event.getPlayer();
+        Location location = player.getLocation();
+        Block block = location.getBlock();
+        BoundingBox playerBox = player.getBoundingBox();
+        List<Block> readyLeaves = new ArrayList<>();
 
-            if (leafData.getTilt() == BigDripleaf.Tilt.FULL) {
+        // get the block the player is in, and all adjacent blocks, if any are a dripleaf continue
+        // check if the player is inside a dripleaf, or air, if so continue
+        // check if the player's hitbox, at their feet, is colliding with any dripleafs that are at full tilt
+        // if so, execute the jump code below
 
-                getServer().getScheduler().runTaskLater(this, () -> {
-                    // big jump
-                    Vector vector = new Vector(0, 1.4, 0);
 
-                    // directional
-                    vector.add(leafData.getFacing().getDirection().multiply(-1));
 
-                    player.setVelocity(player.getVelocity().add(vector));
+        // populate list with leaves at full tilt from a 3x3 area around the player
+//        for (Block curBlock : getBlocksInArea(location.clone().add(-1,0,-1),location.clone().add(1,0,1))) {
+//            if (curBlock.getType() == Material.BIG_DRIPLEAF) {
+//                BigDripleaf leaf = (BigDripleaf) block.getBlockData();
+//                if (leaf.getTilt() == BigDripleaf.Tilt.FULL) {
+//                    readyLeaves.add(leaf);
+//                }
+//            }
+//        }
 
-                    // reset leaf
-                    leafData.setTilt(BigDripleaf.Tilt.NONE);
-                    block.setBlockData(leafData);
-
-                    // sound?
-                    player.playSound(player.getLocation(), Sound.ENTITY_SLIME_ATTACK, 2.0f, 1.0f);
-
-                }, 1);
+        // populate list with leaves at fulltilt that the players feet are colliding with
+        player.sendMessage(String.valueOf(location.getY()));
+        for (Block curBlock : getBlocksInArea(new Location(location.getWorld(),playerBox.getMinX(),location.getY(),playerBox.getMinZ()),new Location(location.getWorld(),playerBox.getMaxX(),location.getY(),playerBox.getMaxZ()))) {
+            if (curBlock.getType() == Material.BIG_DRIPLEAF) {
+                BigDripleaf leaf = (BigDripleaf) block.getBlockData();
+                if (leaf.getTilt() == BigDripleaf.Tilt.FULL) {
+                    readyLeaves.add(curBlock);
+                }
             }
         }
+
+        // if there are none ignore the event
+        if (readyLeaves.isEmpty()) {
+            return;
+        }
+
+        // check to see if the player is standing in air, or a big leaf, if not ignore the event.
+        if (!(block.getType() == Material.BIG_DRIPLEAF || block.getType() == Material.AIR)) {
+            return;
+        }
+
+        // Yeet the player for each leaf
+        for (Block leaf : readyLeaves) {
+            BigDripleaf leafData = (BigDripleaf) leaf.getBlockData();
+            yeet(player, leaf, leafData);
+        }
     }
+
+    private void yeet(Player player,Block block,BigDripleaf leafData) {
+        getServer().getScheduler().runTaskLater(this, () -> {
+            // big jump
+            Vector vector = new Vector(0, 1.4, 0);
+
+            // directional
+            vector.add(leafData.getFacing().getDirection().multiply(-1));
+
+            player.setVelocity(player.getVelocity().add(vector));
+
+            // reset leaf
+            leafData.setTilt(BigDripleaf.Tilt.NONE);
+            block.setBlockData(leafData);
+
+            // sound?
+            player.playSound(player.getLocation(), Sound.ENTITY_SLIME_ATTACK, 2.0f, 1.0f);
+
+        }, 1);
+    }
+
+    // Returns an array with a list of blocks in a designated area
+    public List<Block> getBlocksInArea(Location minLocation, Location maxLocation) {
+        List<Block> blocks = new ArrayList<>();
+
+        for (int x = minLocation.getBlockX(); x <= maxLocation.getBlockX(); x++) {
+            for (int y = minLocation.getBlockY(); y <= maxLocation.getBlockY(); y++) {
+                for (int z = minLocation.getBlockZ(); z <= maxLocation.getBlockZ(); z++) {
+                    Location currentLocation = new Location(minLocation.getWorld(), x, y, z);
+                    Block currentBlock = currentLocation.getBlock();
+
+                    blocks.add(currentBlock);
+                }
+            }
+        }
+        return blocks;
+    }
+
+
 }
+
